56ed98909d14433984a3062e298124fd83210f73
youmu <youmu@DESKTOP-B3PL980>
2022-02-15 09:59:14 +0900

    1

---
+++ msa_cicd/src/main/java/com/example/demo1/MsaCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaCicdApplication.class, args);
+	}
+
+}
